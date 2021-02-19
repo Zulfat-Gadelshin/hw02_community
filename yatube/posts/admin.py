bdf6b311e8737d6cd84f969e90402261bb7c96dd
Zulfat_Gadelshin <Zulfatgadelshin0106@gmail.com>
2021-02-19 19:10:30 +0300

    pep fix #1

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from .models import Post, Group
 
+
 class PostAdmin(admin.ModelAdmin):
     list_display = ('text', 'pub_date', 'author', 'group')
-
 
 
 admin.site.register(Post, PostAdmin)
