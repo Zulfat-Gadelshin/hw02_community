d5ace48ec513fee5a4e0fda554d5954e88d6a806
Zulfat_Gadelshin <Zulfatgadelshin0106@gmail.com>
2021-02-19 19:06:09 +0300

    test commit

---
+++ yatube/posts/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Post, Group
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('text', 'pub_date', 'author', 'group')
+
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Group)
